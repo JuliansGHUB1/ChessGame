c8b2a028df0f02491a8ee1ca0b58b352d7141643
JuliansGHUB1 <25julianbiju@gmail.com>
2024-05-28 11:48:33 -0400

    some additions

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,4 +1,11 @@
+
 class Pawn < Piece
+    @@pawnUnit = 1
+    attr_reader :pawnHasMoved
+    attr_reader :enPassantCapturable
+    attr_writer :pawnHasMoved
+    attr_writer :enPassantCapturable
+
     def initialize(position, color)
         if(color == :White)
             image = "♙"
@@ -7,6 +14,13 @@ class Pawn < Piece
         end
 
        super(position, color, image)
+
+       ## Will be updated by the game
+       @enPassantCapturable = false
+
+       ## Will be updated by the game
+       @pawnHasMoved = false
+
     end
 
     def movementDir
@@ -18,6 +32,84 @@ class Pawn < Piece
             movementDirections = [-1, 0]
             return movementDirections
         end
+
+    end
+
+    def pseudoLegalMoves(gameBoard)
+        print "position[0] " + position[0].to_s + "\n"
+        ## Data structure containing the pseudoLegalMoves
+        pseudoMovesSet = Set.new()
+
+        ## Is pawn forward legal? If so, add it in. 
+        currX, currY = position
+        ## Var representing how the pawn advances, +1 in row or -1 in row
+        rowIncrementor = movementDir[0]
+        ## Then, the location of the next forward square is
+        nextSquare = [currX + rowIncrementor, currY]
+        ## Check if this square will produce out-of-bounds (i.e. is it on our board?)
+        isInBounds = gameBoard.inBounds(nextSquare)
+        ## Pawn forward is legal if the next square is empty
+        if(isInBounds && gameBoard.isEmptySquare(nextSquare))
+            print ("The next square is " + nextSquare[0].to_s + " " + nextSquare[1].to_s)
+            pseudoMovesSet.add(nextSquare)
+            ## Moving two forward is only valid if (1) we can legally move 1 forward and (2) our pawn has not been moved yet 
+            ## (3) The square two-forward is empty
+            if(!pawnHasMoved && gameBoard.isEmptySquare([currX + 2 * rowIncrementor, currY]))
+                ## Here, no need to check if this square is in-bounds
+                pseudoMovesSet.add([currX + 2 * rowIncrementor, currY])
+            end
+        end
+
+
+        ## Are the diagonals containing enemy pieces? If so add these captures
+
+        ## First obtain the diagonal squares this pawn is attacking
+
+        ## Conceptually, the squares a pawn controls are the squares to the right and left of the square should we advance the pawn forward by 1
+        firstDiagonal = [currX + rowIncrementor, currY + 1]
+        secondDiagonal = [currX + rowIncrementor, currY - 1]
+
+        firstDiagonalInBounds = gameBoard.inBounds(firstDiagonal)
+        secondDiagonalInBounds = gameBoard.inBounds(secondDiagonal)
+
+        ## If the diagonal is on the board, and theres an enemy piece on it, the pawn is attacking the enemy piece and threatening to take
+        if(firstDiagonalInBounds && !gameBoard.isEmptySquare(firstDiagonal) && gameBoard.pieceColor(firstDiagonal) != color)
+            pseudoMovesSet.add(firstDiagonalInBounds)
+        end
+
+        if(secondDiagonalInBounds && !gameBoard.isEmptySquare(secondDiagonal) && gameBoard.pieceColor(secondDiagonal) != color)
+            pseudoMovesSet.add(firstDiagonalInBounds)
+        end
+        
+
+
+        ## Is the pawn white and on the 5th rank? Black and on the 4th rank? Check the squares on the neighbouring rows.
+        ## If the en-passant capturable flag is true for these squares, then these are valid moves as well. 
+        if( (color == :White && position[0] = 4) || (color == :Black && position[0]== 3))
+            print "\n Here for piece with color " + color.to_s + " and position " + position[0].to_s + "\n"
+            ## Get squares to right and left
+            rightSquare = [currX, currY + 1]
+            leftSquare = [currX, currY - 1]
+
+            ## You can't en pasant if the square is out of bounds
+            rightSquareInBounds = gameBoard.inBounds(rightSquare)
+            leftSquareInBounds = gameBoard.inBounds(leftSquare)
+
+            ## You can en-pasant if there is an enemy PAWN AND the enemy PAWN is enpasant capturable
+
+            if(!gameBoard.isEmptySquare(rightSquare) && gameBoard.pieceColor(rightSquare) != color && gameBoard.pieceAt(rightSquare).is_a?(Pawn) && gameBoard.pieceAt(rightSquare).enPassantCapturable = true)
+                pseudoMovesSet.add([currX + rowIncrementor, currY + 1])
+            end
+
+            if(!gameBoard.isEmptySquare(leftSquare) && gameBoard.pieceColor(leftSquare) != color && gameBoard.pieceAt(leftSquare).is_a?(Pawn) && gameBoard.pieceAt(leftSquare).enPassantCapturable = true)
+                pseudoMovesSet.add([currX + rowIncrementor, currY - 1])
+            end
+
+            
+
+        end
+
+        return pseudoMovesSet
 
     end
 
--- main.rb
+++ main.rb
@@ -9,6 +9,10 @@ b = Board.new()
 
 k = King.new([7,1], :Black)
 
+k1 = King.new([5, 1], :White)
+
+b.placePiece([5,1], k1)
+
 b.placePiece([7, 1], k)
 
 r = Rook.new([0,7], :Black)
@@ -19,13 +23,21 @@ r1 = Rook.new([0,0], :Black)
 
 ##b.placePiece([3,5], r2)
 
-p1 = Pawn.new([6, 0], :White)
+p1 = Pawn.new([6, 1], :White)
 
- b.placePiece([6,0], p1)
+ b.placePiece([6,1], p1)
 
 b.placePiece([0,7], r)
 
 b.placePiece([0,0], r1)
+
+p2 = Pawn.new([4,0], :White)
+
+p3 = Pawn.new([4,1], :Black)
+
+b.placePiece([4,0], p2)
+
+b.placePiece([4,1], p3)
 
 
 iterable = r1.pseudoLegalMoves(b, 8)
@@ -38,6 +50,11 @@ end
 
 
 print ("\n is the king in check? " + k.isCheck(b).to_s)
+
+
+p p2.pseudoLegalMoves(b)
+
+p p3.pseudoLegalMoves(b)
 
 render = BoardRender.new(b)
 render.render()
